ad5991bd217080e99882c42c1120b9ad38a6ebc2
dbzvishal <dbzvishal@yahoo.com>
2016-09-17 19:21:28 -0700

    Removing admins references in the project

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :buildings
   resources :bookings
-  resources :admins
   resources :users
   resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

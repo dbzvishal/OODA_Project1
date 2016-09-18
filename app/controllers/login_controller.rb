6f38f76b6ccfafbcf8af2c6c6131490bcf10e515
dbzvishal <dbzvishal@yahoo.com>
2016-09-17 21:21:04 -0700

    Basic login page is completed with authentication

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,14 @@ class LoginController < ApplicationController
   end
 
   def show
+    @current_user = User.where("(uemail = ? OR uname = ?) AND password = ?", params[:user][:uname], params[:user][:uname], params[:user][:pass])
 
+    respond_to do |format|
+      if @current_user.empty?
+        format.html { redirect_to root_path, notice: 'Username/Password was not entered correctly.' + params[:uname].to_s }
+      else
+        format.html
+      end
+    end
   end
 end

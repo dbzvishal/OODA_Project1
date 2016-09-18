ca6ca4b5e596f605cfd1622d144e12388f893788
dbzvishal <dbzvishal@yahoo.com>
2016-09-17 18:21:45 -0700

    Dropping admins table

--- db/migrate/20160918011249_drop_admins_table.rb
+++
@@ -1,5 +0,0 @@
-class DropAdminsTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :admins
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918011249) do
+ActiveRecord::Schema.define(version: 20160918010923) do
 
   create_table "bookings", force: :cascade do |t|
     t.date     "date"

0f1a172ecdf30b5e4fd8046c64f1ee5837187d3b
asingh26 <asingh26@ncsu.edu>
2016-09-24 13:27:38 -0400

    Timezone set to eastern.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :logoutAuth
-
   protected
   def logoutAuth(is_redirect=true)
+    @zone='Eastern Time (US & Canada)'
     @admin = false
     @super_admin = false
     @user = User.find_by(id: session[:user_id])

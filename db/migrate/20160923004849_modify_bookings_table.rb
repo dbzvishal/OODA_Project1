9b9cf447f23498c0677e564bd99a157eedf7a665
dbzvishal <dbzvishal@yahoo.com>
2016-09-25 07:48:00 -0700

    Changes to deploy to Heroku - Migration file

--- db/migrate/20160923004849_modify_bookings_table.rb
+++ db/migrate/20160923004849_modify_bookings_table.rb
@@ -1,13 +1,17 @@
 class ModifyBookingsTable < ActiveRecord::Migration[5.0]
   def up
     remove_column :bookings, :date
-    change_column :bookings, :timefrom, :datetime
-    change_column :bookings, :timeto, :datetime
+    remove_column :bookings, :timefrom
+    remove_column :bookings, :timeto
+    add_column :bookings, :timefrom, :datetime
+    add_column :bookings, :timeto, :datetime
   end
 
   def down
     add_column :bookings, :date, :date
-    change_column :bookings, :timefrom, :time
-    change_column :bookings, :timeto, :time
+    remove_column :bookings, :timefrom
+    remove_column :bookings, :timeto
+    add_column :bookings, :timefrom, :time
+    add_column :bookings, :timeto, :time
   end
 end
